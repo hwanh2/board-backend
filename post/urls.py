1bcb4262936624c5a6be6b1f1e29a8dc89f3b7f4
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-06-25 02:26:42 +0900

    refacotor: url 구조 변경

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', PostView.as_view(), name='post_list_create'),
     path('<int:post_id>', PostDetailView.as_view(), name='post_detail'),
-    
+    path('<int:post_id>/comments', CommentView.as_view(), name='comment_create'),
 ]
