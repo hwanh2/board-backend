206e5c9832bd291ed73cdc0cd0c79bc776ef28e0
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-06-29 18:50:31 +0900

    refactor: 시리얼라이저 수정

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,11 +14,17 @@ class CommentCreateSerializer(serializers.ModelSerializer):
         post = self.context['post']
         return Comment.objects.create(user_id=user, post_id=post, **validated_data)
 
+class UserSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSimpleSerializer(source='user_id', read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'user_id', 'created_at', 'updated_at']
+        fields = ['id', 'content', 'user', 'created_at', 'updated_at']
 
 class CommentSummarySerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user_id.username')
